5b311791ea6189a8a9cf13f78f069626741dfe92
xuxianhua <xastra@qq.com>
2018-05-16 09:55:09 +0800

    init commit

---
+++ mpeg2dec/mpeg2decoder.h
@@ -0,0 +1,8 @@
+//////////////////////////////////////////////////////////////////////////
+//mpeg2Decoder.h : main header file for the mpeg2DECODER DLL
+
+#ifdef __cplusplus
+#define EXPORT extern "C" __declspec (dllexport)
+#else
+#define EXPORT __declspec (dllexport)
+#endif 
